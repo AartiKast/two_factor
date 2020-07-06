13ef039bc9e3ecc08337f5e37cbf914e7a88b221
Aarti Maheshwari <aarti.m@yudiz.in>
2020-07-06 18:45:45 +0530

    middleware changes

--- src/routes/routes.php
+++ src/routes/routes.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Shree\TwoFactor\Http\Controllers\TwoFactorController;
+use Shree\TwoFactor\Http\Middleware\TwoFactorVerification;
 
-Route::group(['middleware' => ['TwoFactorVerification']],function(){
+Route::group(['middleware' => [TwoFactorVerification::class]],function(){
 	
 	Route::get('/2fa',[TwoFactorController::class,'show2faForm']);
 	Route::post('/2fa-verify',[TwoFactorController::class,'verifyToken']);
