63a1560c57cf4698411e4cebb1b242c9fc90317c
Aarti Maheshwari <aarti.m@yudiz.in>
2020-07-06 11:47:59 +0530

    config file

--- src/Provider/TwoFactorServiceProvider.php
+++ src/Provider/TwoFactorServiceProvider.php
@@ -9,21 +9,21 @@ use Shree\TwoFactor\Http\Middleware\TwoFactorVerification;
 class TwoFactorServiceProvider extends ServiceProvider{
 	public function boot(){
 		$this->loadRoutesFrom(__DIR__ . '/routes/routes.php');
-		$this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-		$this->loadViewsFrom(__DIR__.'/resources/views','2fa');
+		$this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+		$this->loadViewsFrom(__DIR__.'/../../resources/views','2fa');
 
 		$router = $this->app->make(Router::class);
 		$router->aliasMiddleware('TwoFactorVerification',TwoFactorVerification::class);
 
 		if($this->app->runningInConsole()){
 			//publish config file
-			$this->publishes([__DIR__.'/../config/twoFactorConfig.php' => config_path('twoFactorConfig.php'),],'config');
+			$this->publishes([__DIR__.'/../../config/twoFactorConfig.php' => config_path('twoFactorConfig.php'),],'config');
 
 			//publish migration file
-			$this->publishes([__DIR__.'/../database/migrations/' => database_path('migrations'),],'migrations');
+			$this->publishes([__DIR__.'/../../database/migrations/' => database_path('migrations'),],'migrations');
 
 			//publish view files
-			$this->publishes([__DIR__.'/../resources/views/' => resource_path('views/vendor/2fa'),],'views');
+			$this->publishes([__DIR__.'/../../resources/views/' => resource_path('views/vendor/2fa'),],'views');
 		}
 	}
 
